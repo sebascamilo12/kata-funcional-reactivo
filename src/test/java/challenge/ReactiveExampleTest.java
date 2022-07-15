d40e013e8e6890775c9233f46fd01ced4925e949
Sebastian Pineda <63763483+sebascamilo12@users.noreply.github.com>
2022-07-15 17:54:34 -0500

    Se crean los test

--- src/test/java/challenge/ReactiveExampleTest.java
+++ src/test/java/challenge/ReactiveExampleTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.List;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -14,32 +19,71 @@ class ReactiveExampleTest {
 
 
     @Test
-    void sumaDePuntajes(){
+    void sumaDePuntajes() {
+        StepVerifier
+                .create(reactiveExample.sumaDePuntajes())
+                .expectNext(260)
+                .verifyComplete();
+    }
+
+    @Test
+    void mayorPuntajeDeEstudiante() {
+        Flux<Estudiante> flux = reactiveExample.mayorPuntajeDeEstudiante(1);
+        StepVerifier
+                .create(flux)
+                .expectNextMatches(estudiante -> estudiante.getNombre().equals("pedro"))
+                .verifyComplete();
 
     }
 
     @Test
-    void mayorPuntajeDeEstudiante(){
+    void totalDeAsisntenciasDeEstudiantesComMayorPuntajeDe() {
+        StepVerifier
+                .create(reactiveExample.totalDeAsisntenciasDeEstudiantesConMayorPuntajeDe(75))
+                .expectNext(43)
+                .verifyComplete();
 
     }
 
     @Test
-    void totalDeAsisntenciasDeEstudiantesComMayorPuntajeDe(){
+    void elEstudianteTieneAsistenciasCorrectas() {
+        var valor = reactiveExample.elEstudianteTieneAsistenciasCorrectas(
+                new Estudiante("andres", 35, List.of(4, 2, 4, 3, 5)));
+        StepVerifier
+                .create(valor)
+                .expectNext(true)
+                .verifyComplete();
+
 
     }
 
     @Test
-    void elEstudianteTieneAsistenciasCorrectas(){
+    void promedioDePuntajesPorEstudiantes() {
+
+        StepVerifier
+                .create(reactiveExample.promedioDePuntajesPorEstudiantes())
+                .expectNext(52.0)
+                .verifyComplete();
 
     }
 
     @Test
-    void promedioDePuntajesPorEstudiantes(){
+    void estudiantesAprovados() {
 
+        StepVerifier
+                .create(reactiveExample.estudiantesAprovados())
+                .expectNext("juan")
+                .expectNext("pedro")
+                .verifyComplete();
     }
 
     @Test
-    void estudiantesAprovados(){
+    void losNombresDeEstudianteConPuntajeMayorA() {
+        StepVerifier
+                .create(reactiveExample.losNombresDeEstudianteConPuntajeMayorA(75))
+                .expectNext("juan")
+                .expectNext("pedro")
+                .verifyComplete();
 
     }
 }
